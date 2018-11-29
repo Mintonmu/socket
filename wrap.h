bbf3a1228ced3b396e03ff25eeece88fbbe7706d
Venom <2859595067@qq.com>
2018-11-29 15:48:19 +0800

    Update wrap.h

--- wrap.h
+++ wrap.h
@@ -10,9 +10,5 @@ int Socket(int family, int type, int protocol);
 ssize_t Read(int fd, void *ptr, size_t nbytes);
 ssize_t Write(int fd, const void *ptr, size_t nbytes);
 int Close(int fd);
-ssize_t Readn(int fd, void *vptr, size_t n);
-ssize_t Writen(int fd, const void *vptr, size_t n);
-ssize_t my_read(int fd, char *ptr);
-ssize_t Readline(int fd, void *vptr, size_t maxlen);
 
 #endif
